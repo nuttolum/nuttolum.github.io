2f545974adabbcad005a9a241f703d6c7e4d3a49
nuttolum <nuttolum@gmail.com>
2022-04-21 17:19:17 -0400

    w

--- FFMPEG/ffmpeg.js
+++ FFMPEG/ffmpeg.js
@@ -1,40 +1,43 @@
 //require ffmpeg
-const ffmpeg = require('fluent-ffmpeg');
-var ffmpegPath = require("ffmpeg-static")
-var ffprobePath = require("ffprobe-static").path
-ffmpeg.setFfmpegPath(ffmpegPath)
-ffmpeg.setFfprobePath(ffprobePath)
-const { getVideoDurationInSeconds } = require('get-video-duration')
-const fileSize = 8
-const percentError = .15
-function getBitrate(videoLength) {
-    const finalFileSize = fileSize - (fileSize * percentError)
-   var videoBitRate = (finalFileSize * 0.5)/(videoLength * .0075)
-   var audioBitRate = (finalFileSize*0.5)/((videoLength*60) * 2) * 1000
-   return {videoBitRate, audioBitRate}
-}
+const ffmpeg = require("fluent-ffmpeg");
+var ffmpegPath = require("ffmpeg-static");
+var ffprobePath = require("ffprobe-static").path;
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobePath);
 
-async function getVideoLength(path) {
-    const timeInSeconds = await getVideoDurationInSeconds(path)
-    return timeInSeconds / 60
-}
-const videoPath = "./test.mp4"
+const sizeInBits = 8 * 1024 * 1024 * 8;
+const videoAudioRatio = 0.75;
+
+const videoPath = "./test.mp4";
 async function main() {
-const {videoBitRate, audioBitRate} = getBitrate(await getVideoLength(videoPath))
-console.log(videoBitRate, audioBitRate)
-var command = ffmpeg(videoPath)
-.audioBitrate(audioBitRate)
-.videoBitrate(videoBitRate)
-.outputOptions([
-    `-bufsize ${videoBitRate}k`,
-])
-var save = command.save('./output.mp4')
-save.on("progress", function(progress) {
-    console.log(progress.percent + "% done");
+  ffmpeg.ffprobe(videoPath, function (err, metadata) {
+    if (err) throw err;
+    const isSmallerThan8mb = metadata.format.size / 1024 / 1024 < 8;
+    if (isSmallerThan8mb) {
+      console.log("Video is smaller than 8mb");
+      return;
+    } else {
+      const videoDuration = Math.floor(metadata.format.duration);
+      const totalBitrate = sizeInBits / videoDuration;
+      const videoBitrate = totalBitrate * videoAudioRatio;
+      const audioBitrate = totalBitrate - videoBitrate;
+      ffmpeg(videoPath)
+        .outputOptions([
+          "-c:v libx264",
+          "-b:v " + videoBitrate,
+          "-maxrate " + videoBitrate,
+          "-bufsize:v " + videoBitrate / 5,
+          "-c:a aac",
+          "-b:a " + audioBitrate,
+          "-pass 1",
+        ])
+        .saveToFile("./output.mp4")
+        .on("progress", function (progress) {
+          //print progress.percent as a percent with 2 decimal places
+          console.log(Math.floor(progress.percent * 100) / 100 + "% done");
+        });
+    }
   });
-    save.on("end", function() {
-        console.log("Finished processing");
-    });
 }
 
-main()
+main();
