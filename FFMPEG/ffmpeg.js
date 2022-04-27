02ddf43c20a67f0178f930b6ec24f27ccdc450ef
nuttolum <nuttolum@gmail.com>
2022-04-26 22:54:38 -0400

    stuff

--- FFMPEG/ffmpeg.js
+++ FFMPEG/ffmpeg.js
@@ -8,35 +8,34 @@ ffmpeg.setFfprobePath(ffprobePath);
 const sizeInBits = 8 * 1024 * 1024 * 8;
 const videoAudioRatio = 0.75;
 
-const videoPath = "./test.mp4";
+const videoPath = "./test2_full.mp4";
 async function main() {
   ffmpeg.ffprobe(videoPath, function (err, metadata) {
     if (err) throw err;
     const isSmallerThan8mb = metadata.format.size / 1024 / 1024 < 8;
-    if (isSmallerThan8mb) {
-      console.log("Video is smaller than 8mb");
-      return;
-    } else {
       const videoDuration = Math.floor(metadata.format.duration);
       const totalBitrate = sizeInBits / videoDuration;
       const videoBitrate = totalBitrate * videoAudioRatio;
       const audioBitrate = totalBitrate - videoBitrate;
       ffmpeg(videoPath)
+        //compress to 8mb
         .outputOptions([
           "-c:v libx264",
-          "-b:v " + videoBitrate,
-          "-maxrate " + videoBitrate,
-          "-bufsize:v " + videoBitrate / 5,
+          "-crf 18",
+          "-preset veryfast",
           "-c:a aac",
           "-b:a " + audioBitrate,
-          "-pass 1",
+          "-b:v " + videoBitrate,
+          "-maxrate " + videoBitrate,
+          "-bufsize " + videoBitrate/ 5,
         ])
+
         .saveToFile("./output.mp4")
         .on("progress", function (progress) {
           //print progress.percent as a percent with 2 decimal places
           console.log(Math.floor(progress.percent * 100) / 100 + "% done");
         });
-    }
+    
   });
 }
 
